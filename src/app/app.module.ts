1cd62857fc8614656ef2f2215a51e415f8a80d16
NaveedAslam2029 <54169413+NaveedAslam2029@users.noreply.github.com>
2020-04-26 06:55:36 -1200

    CHANGES URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,7 @@ import { environment } from '../environments/environment';
       strategies: [
         NbPasswordAuthStrategy.setup({
           name: 'email',
-          baseEndpoint: environment.backendUrl,
+          baseEndpoint:https://ultimatesmsbackend.herokuapp.com/,
           login: {
             endpoint: '/user/login',
             method: 'post',
